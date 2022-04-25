79e29c2171cba61cb0c7cec90df572071e583ca4
Kalvin Pearce <kalvinpearce@gmail.com>
2022-04-26 00:31:58 +1000

    refactor: adjust user rules heading

--- lua/gitignore-gen/config.lua
+++ lua/gitignore-gen/config.lua
@@ -4,6 +4,6 @@ M.api_url = "https://www.toptal.com/developers/gitignore/api/"
 M.filename = ".gitignore"
 M.main_header = "Generated with gitignore-gen.nvim"
 M.user_rules_header =
-	"Custom rules (everything added below won't be overriden by 'Generate .gitignore File' if you use 'Update' option)"
+	"Custom rules (everything added below won't be overriden when running gitignore-gen.generate)"
 
 return M
